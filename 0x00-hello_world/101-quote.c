05bbec8d432e44e09322dee276645c6b832ea701
Arjennanjaya <nanjayaian@gmail.com>
2023-02-17 03:58:02 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - a program that prints a line to a standard error
  * Return: should return 1 when successfully executed
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, '2015-10-19\n" 59);
-	return (1);
+fprintf(stderr, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+return (1);
 }
