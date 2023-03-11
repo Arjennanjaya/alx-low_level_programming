8aa576177861f3003a453ec404895251b7a32e5b
Arjennanjaya <nanjayaian@gmail.com>
2023-03-11 01:09:10 -0800

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -6,12 +6,12 @@
  */
 int _abs(int c)
 {
-        if (c < 0)
-        {
-        int abs_value;
+	if (c < 0)
+	{
+	int abs_value;
 
-        abs_value = c * -1;
-        return (abs_value);
-        }
-        return (c);
+	abs_value = c * -1;
+	return (abs_value);
+	}
+	return (c);
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -8,17 +8,17 @@
 
 char *_strcpy(char *dest, char *src)
 {
-        int l = 0;
-        int x = 0;
+	int l = 0;
+	int x = 0;
 
-        while (*(src + 1) != '\0')
-        {
-                l++;
-        }
-        for (; x < l; x++)
-        {
-                dest[x] = src[x];
-        }
-        dest[1] = '\0';
-        return (dest);
+	while (*(src + 1) != '\0')
+	{
+		l++;
+	}
+	for (; x < l; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
 }
